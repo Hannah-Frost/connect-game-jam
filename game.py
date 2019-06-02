6411891dd8a9141e0c4eae2663dda97fdd4546b0
Hannah Frost <h.7frost@gmail.com>
2019-06-02 18:27:46 +0100

    Added basic play button

--- game.py
+++ game.py
@@ -4,16 +4,30 @@ import os
 def main():
     pygame.init()
     pygame.display.set_caption("Hello B)")
+    screen = pygame.display.set_mode((800,600))
 
-    screen = pygame.display.set_mode((640,480))
-    image = pygame.image.load(os.path.join("images", "background.png"))
-    screen.blit(image, (5,5))
-    pygame.display.flip()
+    menu_background = pygame.image.load(os.path.join("images", "background.png"))
+    play_button = pygame.image.load(os.path.join("images", "basic_button.png")).convert()
+
+    game_background = pygame.image.load(os.path.join("images", "game_background.png"))
 
     running = True
+    MENU = True
+    GAME = False
 
     while running:
+        if MENU:
+            screen.blit(menu_background, (5,5))
+            screen.blit(play_button,(600, 40))
+            pygame.display.flip()
+        if GAME:
+            screen.blit(game_background, (0,0))
+            pygame.display.flip()
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                if play_button.get_rect().collidepoint(x, y):
+                    print("IMAGE WAS CLICKED")
             if event.type == pygame.QUIT:
                 running = False
 if __name__ =="__main__":
