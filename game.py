cf37fb9ae1543e1a9a10f5c7468f6a1686df6d18
Hannah Frost <h.7frost@gmail.com>
2019-06-02 20:27:17 +0100

    Play button loads blank page

--- game.py
+++ game.py
@@ -7,28 +7,32 @@ def main():
     screen = pygame.display.set_mode((800,600))
 
     menu_background = pygame.image.load(os.path.join("images", "background.png"))
-    play_button = pygame.image.load(os.path.join("images", "basic_button.png")).convert()
-
-    game_background = pygame.image.load(os.path.join("images", "game_background.png"))
+    play_button_image = pygame.image.load(os.path.join("images", "basic_button.png")).convert()
+    play_button = pygame.draw.rect(screen, (190, 190, 255), [500, 200, 121, 61])
+    character_selection = pygame.image.load(os.path.join("images", "character_selection.png"))
 
     running = True
     MENU = True
+    SELECTION = False
     GAME = False
 
     while running:
         if MENU:
             screen.blit(menu_background, (5,5))
-            screen.blit(play_button,(600, 40))
+            play_button = pygame.draw.rect(screen, (190, 190, 255), [600, 40, 121, 61])
+            screen.blit(play_button_image, [600, 40])
             pygame.display.flip()
-        if GAME:
-            screen.blit(game_background, (0,0))
+        if SELECTION:
+            screen.blit(character_selection, (0,0))
             pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = event.pos
-                if play_button.get_rect().collidepoint(x, y):
-                    print("IMAGE WAS CLICKED")
+                if play_button.collidepoint(x, y):
+                    SELECTION = True
+                    MENU = False
             if event.type == pygame.QUIT:
                 running = False
+
 if __name__ =="__main__":
     main()
